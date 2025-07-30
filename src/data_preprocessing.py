9ec01e369dc424a3c6ffdda1e24656f7a56c18fc
Shreyasonawane <148079921+Shreyasonawane@users.noreply.github.com>
2025-07-30 14:09:14 +0530

    Delete src directory

--- src/data_preprocessing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-
-def preprocess_data(path):
-    df = pd.read_csv(path)
-    df['Amount'] = StandardScaler().fit_transform(df[['Amount']])
-    df = df.drop(['Time'], axis=1)
-    return df
