3fd3d2906fdf0ade3828648303411fc28d227c67
Shreyasonawane <148079921+Shreyasonawane@users.noreply.github.com>
2025-08-01 22:38:59 +0530

    Delete Drone Monitoring for Traffic and Road Condition/utils.py

--- Drone Monitoring for Traffic and Road Condition/utils.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-def draw_boxes(frame, box, track_id):
-    x, y, w, h = box
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv2.putText(frame, f"ID: {track_id}", (x, y - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
